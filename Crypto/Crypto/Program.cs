e812b8772797b39b7a876f014bb2200d6e220200
ocph23 <ocph23@gmail.com>
2018-11-01 18:19:44 +0900

    #@

--- Crypto/Crypto/Program.cs
+++ Crypto/Crypto/Program.cs
@@ -1,116 +1,117 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
-namespace Crypto
+namespace crypto
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static string keys = abc.ToLower();
+        private static string key = "abz";
+
+        public static void Main(string[] args)
         {
-            bool complete = false;
-            while (!complete)
+
+            var enc = ENCRYPT("SAYAPERGIKEPASAR", key);
+
+
+            Console.WriteLine(enc);
+
+            var decr = DECRYPT(enc, key);
+
+
+            Console.WriteLine(decr);
+
+
+            var ct = "TGCSZ GEUAA EFWGQ AHQMC".Split(" ");
+            //  Console.WriteLine(DECRYPT(ct,"muk"));
+            //
+            string[] digits = abc.Select(o => o.ToString()).ToArray();
+
+
+
+            int N = 3;
+            int D = abc.Length;
+            int[] digit = new int[N];
+            var q = new List<string>();
+        again:
+            var aaa = string.Join("", digit.Reverse().Select(x => char.ConvertFromUtf32(65 + x)).ToArray());
+            Console.Write("{0} ", aaa); // 65 is 'A'
+            for (int i = 0; i < N; ++i)
             {
-                var proccessType = ProccessType.Encrypt;
-                var sourceType = SourceType.Alphabeth;
-
-                Console.Write("Crypto Type (encrypt/decrypt) : "); var cryptoType = Console.ReadLine();
-
-                if (cryptoType.ToUpper() == "DECRYPT")
-                    proccessType = ProccessType.Decript;
-                else
-                    cryptoType = "ENCRYPT";
-
-
-                Console.Clear();
-                Console.WriteLine("Crypto Type : "+ cryptoType);
-                Console.Write("Input Your Text : "); var text = Console.ReadLine();
-                Console.WriteLine();
-               
-                if (!text.All(s=>char.IsLetter(s)))
-                    sourceType = SourceType.ASCII;
-
-                Console.WriteLine("Result");
-                Console.WriteLine("Source Type : " + sourceType.ToString().ToUpper());
-             
-                try
-                {
-                    switch (proccessType)
-                    {
-                        case ProccessType.Encrypt:
-                            var encryptResult = Proccess(text, 1000, sourceType, proccessType);
-                            Console.WriteLine("Encrypt Result: " + encryptResult);
-                            break;
-
-                        case ProccessType.Decript:
-                            var decriptResult = Proccess(text, 1000, sourceType, ProccessType.Decript);
-                            Console.WriteLine("Decrypt Result: " + decriptResult);
-                            break;
-                    }
-                    Console.Write("Keluar ? (Y/N) : ");
-                    var isOut = Console.ReadLine();
-                    if (isOut.ToUpper()=="Y")
-                    {
-                        complete = true;
-                    }
-                    Console.Clear();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                if (++digit[i] < D)
+                    goto again;
+                digit[i] = 0;
+                q.Add(aaa);
             }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine(Math.Pow(27, N) + " - " + q.Count());
+            var r = 0;
+            foreach (var item in q)
+            {
+                Console.WriteLine(DECRYPT(ct[0], item.ToLower()) + " " + DECRYPT(ct[1], item.ToLower()) + " " + DECRYPT(ct[2], item.ToLower()) + "  " + item);
+            }
+            return;
         }
 
-        private static string Proccess(string text, int key, SourceType type, ProccessType proccessType)
+        private static string ENCRYPT(string pt, string key)
         {
             StringBuilder sb = new StringBuilder();
-            switch (type)
+            var keyIndex = 0;
+            foreach (var item in pt)
             {
-                case SourceType.Alphabeth:
-                    var plainText = text.ToUpper();
-                    var source = GetAlphabet().ToUpper();
-                    foreach (var item in plainText)
-                    {
-                        int resultIncex = GetIndex(source.IndexOf(item), key, source.Length, proccessType);
-                        sb.Append(source[resultIncex]);
-                    }
-                    break;
+                if (keyIndex >= key.Length)
+                    keyIndex = 0;
 
-                case SourceType.ASCII:
-                    byte[] values = Encoding.ASCII.GetBytes(text);
-                    foreach (var item in values)
-                    {
-                        int resultIncex = GetIndex(item, key, 256, proccessType);
-                        sb.Append((char)resultIncex).ToString();
-                    }
-                    break;
+                var result = mod((Index(item) + IndexKey(key[keyIndex])), 26);
+                sb.Append(abc[result]);
+
+                keyIndex++;
             }
 
             return sb.ToString();
-
         }
 
-        private static string GetAlphabet()
+        private static string DECRYPT(string pt, string key)
         {
-            return "abcdefghijklmnopqrstuvwxyz";
-        }
+            StringBuilder sb = new StringBuilder();
+            var keyIndex = 0;
+            foreach (var item in pt)
+            {
+                if (keyIndex >= key.Length)
+                    keyIndex = 0;
 
-        private static int GetIndex(int index, int key, int length, ProccessType proccessType)
-        {
-            if (proccessType == ProccessType.Encrypt)
-                return Program.mod((index + key), length);
-            else
-                return Program.mod((index - key), length);
-        }
+                var result = mod((Index(item) - IndexKey(key[keyIndex])), 26);
+                sb.Append(abc[result]);
 
+                keyIndex++;
+            }
+
+            return sb.ToString();
+        }
 
         public static int mod(int n, int m)
         {
             return ((n % m) + m) % m;
         }
-    }
 
+
+        public static int Index(char huruf)
+        {
+            return abc.IndexOf(huruf);
+        }
+
+
+        public static int IndexKey(char huruf)
+        {
+            return keys.IndexOf(huruf);
+        }
+    }
 
 
 
@@ -123,6 +124,11 @@ namespace Crypto
     {
         Encrypt, Decript
     }
+
+
+
+
+
 
 
 
